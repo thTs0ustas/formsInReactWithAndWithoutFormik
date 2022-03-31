1873867635b3e9ac8f5ec303d7cbbdebaedb38f2
konsavv <ksavvidi@hotmail.com>
2022-03-31 16:44:08 +0300

    Comment the links from footer description

--- src/components/footer/Description.js
+++ src/components/footer/Description.js
@@ -4,11 +4,11 @@ export default function Description() {
   return (
     <div class="description">
       <p>© Copyright 2022 Cameo Cinemas. 1628 Burwood Highway Belgrave 3160</p>
-      <p>
+      {/* <p>
         <a href="#">Terms of use</a>
         <a href="#">Privacy Policy</a>
         <a href="#">Site by Chook</a>
-      </p>
+      </p> */}
     </div>
   );
 }
