ab72cc798607d3fd38b5eeef45543876630a07e9
Kostas Diam <tsingoku@yahoo.gr>
2022-04-18 18:40:16 +0300

    Fixing MoviesOfTheMonth....

--- src/components/moviesOfTheMonth/moviesOfTheMonth.js
+++ src/components/moviesOfTheMonth/moviesOfTheMonth.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
 import PropTypes from "prop-types";
 import axios from 'axios';
@@ -13,13 +13,19 @@ import {
 } from "./styledComponents/styles";
 import { useMoviesOfTheMonth } from "./hooks/useMoviesOfTheMonth";
 import { useProvider } from "../../model";
-import {
-  ComingUp
-} from "../../layouts/homePage/styledComponents/styles";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import { ButtonIcon, CarouselButton } from "./";
 import { map } from "lodash";
 import {Link} from 'react-router-dom';
 
 export const MoviesOfTheMonth = () => {
+
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
 
   useMoviesOfTheMonth();
   const [state, dispatch] = useProvider();
@@ -32,10 +38,13 @@ export const MoviesOfTheMonth = () => {
       <p>See all films & session times</p>
     </TitleHeader>
     <ShowingToday>
+    <Carousel activeIndex={index} onSelect={handleSelect} fade>
       {/* edw tha mpei to 1o map gia to carousel */}
+
       <Row className='flex-nowrap'>
         {map(state.homepage.movies[0], ({ Screenings, Movie: { id, title, genre, image, Screening } }) =>
           <ColStyled key={id} md={4} sm={6} xs={12}>
+        <Carousel.Item key={id} interval={3000}>
             <MoviesMonthImg src={`${state.BASE_URL}${image}`} />
             <p>{genre.replace(/^\w/, (c) => c.toUpperCase())}</p>
             <h2><Link to={`/reservation/${id}`}>{title}</Link></h2>
@@ -44,11 +53,13 @@ export const MoviesOfTheMonth = () => {
                 <MoviesMonthScreeningItem key={id}>{movie_starts.split("T")[1].slice(0, 5)} PM</MoviesMonthScreeningItem>
               )}
             </MoviesMonthScreeningContainer>
+          </Carousel.Item>
           </ColStyled>
         )}
 
 
       </Row>
+      </Carousel>
     </ShowingToday>
     </>
   )
