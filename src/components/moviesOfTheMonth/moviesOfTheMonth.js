2bfc77abbf5f358e82d11cf2ff9c6169a7d988bc
thts0ustas <th.ts0ustas@gmail.com>
2022-04-18 19:25:05 +0300

    slideshow refactor

--- src/components/moviesOfTheMonth/moviesOfTheMonth.js
+++ src/components/moviesOfTheMonth/moviesOfTheMonth.js
@@ -1,66 +1,72 @@
-import { useState, useEffect } from "react";
-import React from "react";
-import PropTypes from "prop-types";
-import axios from 'axios';
-import { Row, Col, NavItem } from 'react-bootstrap';
+import React, { useState } from "react";
 import {
-  ShowingToday,
+  ColStyled,
   MoviesMonthImg,
   MoviesMonthScreeningContainer,
   MoviesMonthScreeningItem,
-  ColStyled,
-  TitleHeader
+  ShowingToday,
+  TitleHeader,
 } from "./styledComponents/styles";
 import { useMoviesOfTheMonth } from "./hooks/useMoviesOfTheMonth";
 import { useProvider } from "../../model";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import { ButtonIcon, CarouselButton } from "./";
-import { map } from "lodash";
-import {Link} from 'react-router-dom';
+import { chunk, map } from "lodash";
+import { Link } from "react-router-dom";
 
 export const MoviesOfTheMonth = () => {
+  const today = new Date().getDay();
+  const movieDate = (movie_starts) => new Date(movie_starts).getDay();
 
   const [index, setIndex] = useState(0);
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
 
-
   useMoviesOfTheMonth();
   const [state, dispatch] = useProvider();
-  console.log(state)
+  let slices = chunk(state.homepage.movies[0], 3);
+  console.log(slices);
 
   return (
     <>
-    <TitleHeader>
-      <h2>FILMS SHOWING TODAY</h2>
-      <p>See all films & session times</p>
-    </TitleHeader>
-    <ShowingToday>
-    <Carousel activeIndex={index} onSelect={handleSelect} fade>
-      {/* edw tha mpei to 1o map gia to carousel */}
-
-      <Row className='flex-nowrap'>
-        {map(state.homepage.movies[0], ({ Screenings, Movie: { id, title, genre, image, Screening } }) =>
-          <ColStyled key={id} md={4} sm={6} xs={12}>
-        <Carousel.Item key={id} interval={3000}>
-            <MoviesMonthImg src={`${state.BASE_URL}${image}`} />
-            <p>{genre.replace(/^\w/, (c) => c.toUpperCase())}</p>
-            <h2><Link to={`/reservation/${id}`}>{title}</Link></h2>
-            <MoviesMonthScreeningContainer>
-              {map(Screenings, ({ id, movie_starts }) =>
-                <MoviesMonthScreeningItem key={id}>{movie_starts.split("T")[1].slice(0, 5)} PM</MoviesMonthScreeningItem>
+      <TitleHeader>
+        <h2>FILMS SHOWING TODAY</h2>
+        <p>See all films & session times</p>
+      </TitleHeader>
+      <ShowingToday>
+        <Carousel activeIndex={index} onSelect={handleSelect} fade>
+          {map(slices, (slice, i) => (
+            <Carousel.Item key={i} interval={3000} style={{ display: "flex" }}>
+              {map(
+                slice,
+                ({
+                  Screenings,
+                  Movie: { id, title, genre, image, Screening },
+                }) => (
+                  <ColStyled key={id} md={4} sm={6} xs={12}>
+                    <MoviesMonthImg src={`${state.BASE_URL}${image}`} />
+                    <p>{genre.replace(/^\w/, (c) => c.toUpperCase())}</p>
+                    <h2>
+                      <Link to={`/reservation/${id}`}>{title}</Link>
+                    </h2>
+                    <MoviesMonthScreeningContainer>
+                      {map(Screenings, ({ id, movie_starts }) => (
+                        <MoviesMonthScreeningItem key={id}>
+                          {movie_starts.split("T")[1].slice(0, 5)} PM
+                        </MoviesMonthScreeningItem>
+                      ))}
+                    </MoviesMonthScreeningContainer>
+                  </ColStyled>
+                )
               )}
-            </MoviesMonthScreeningContainer>
-          </Carousel.Item>
-          </ColStyled>
-        )}
+            </Carousel.Item>
+          ))}
 
-
-      </Row>
-      </Carousel>
-    </ShowingToday>
+          {/*</Row>*/}
+        </Carousel>
+      </ShowingToday>
     </>
-  )
-}
+  );
+};
--- src/components/moviesOfTheMonth/styledComponents/styles.js
+++ src/components/moviesOfTheMonth/styledComponents/styles.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
-import {  Col } from 'react-bootstrap';
-
+import { Col } from "react-bootstrap";
 
 const TitleHeader = styled.div`
   display: flex;
@@ -8,43 +7,42 @@ const TitleHeader = styled.div`
   justify-content: center;
   height: 150px;
   text-align: center;
-  background-color: ${({ theme }) => theme.bgMain };
+  background-color: ${({ theme }) => theme.bgMain};
 
   & h2 {
-    color: ${({ theme }) => theme.secondary };
+    color: ${({ theme }) => theme.secondary};
   }
 
   & p {
-    color: ${({ theme }) => theme.primary };
+    color: ${({ theme }) => theme.primary};
   }
 `;
 
-
 const ShowingToday = styled.div`
-  background-color: ${({ theme }) => theme.bgMain };
+  background-color: ${({ theme }) => theme.bgMain};
   padding: 10px 0;
 
   & p {
     margin-left: 25px;
     font-weigth: 200;
-    color: ${({ theme }) => theme.secondary };
+    color: ${({ theme }) => theme.secondary};
   }
 
   & h2 {
     margin-left: 25px;
     font-weight: bolder;
     text-transform: uppercase;
-    color: ${({ theme }) => theme.white };
+    color: ${({ theme }) => theme.white};
   }
 
   & a {
     text-decoration: none;
     cursor: pointer;
-    color: ${({ theme }) => theme.white };
+    color: ${({ theme }) => theme.white};
   }
 
   & a:hover {
-    color: ${({ theme }) => theme.primary };
+    color: ${({ theme }) => theme.primary};
     -webkit-transition: color 0.3s ease-out 0s;
     -moz-transition: color 0.3s ease-out 0s;
     -ms-transition: color 0.3s ease-out 0s;
@@ -56,9 +54,6 @@ const ShowingToday = styled.div`
     opacity: 1;
   }
 `;
-
-
-
 
 const MoviesMonthImg = styled.img`
   opacity: 0.7;
@@ -79,9 +74,7 @@ const MoviesMonthImg = styled.img`
     width: 93vw;
     height: 88vw;
   }
-
 `;
-
 
 const MoviesMonthScreeningContainer = styled.div`
   display: flex;
@@ -90,11 +83,11 @@ const MoviesMonthScreeningContainer = styled.div`
   flex-wrap: wrap;
   justify-content: flex-start;
   margin-left: 25px;
-  gap:5px;
+  gap: 5px;
 `;
 
 const MoviesMonthScreeningItem = styled.div`
-  background-color: ${({ theme }) => theme.primary };
+  background-color: ${({ theme }) => theme.primary};
   color: #000;
   height: 25px;
   line-height: 25px;
@@ -103,19 +96,20 @@ const MoviesMonthScreeningItem = styled.div`
   font-weight: bold;
 `;
 
-
 const ColStyled = styled(Col)`
-  & h2 {}
-  & p {}
+  display: flex;
+  flex-direction: column;
+  & h2 {
+  }
+  & p {
+  }
 `;
 
-
 export {
-    TitleHeader,
-    ShowingToday,
-    ColStyled,
-    MoviesMonthImg,
-    MoviesMonthScreeningContainer,
-    MoviesMonthScreeningItem,
+  TitleHeader,
+  ShowingToday,
+  ColStyled,
+  MoviesMonthImg,
+  MoviesMonthScreeningContainer,
+  MoviesMonthScreeningItem,
 };
-  
