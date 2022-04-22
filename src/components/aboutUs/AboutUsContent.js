26a012d228317168da75c7e01be58ab39e05ecbb
konsavv <ksavvidi@hotmail.com>
2022-04-22 12:51:23 +0300

    Changed about us

--- src/components/aboutUs/AboutUsContent.js
+++ src/components/aboutUs/AboutUsContent.js
@@ -5,26 +5,26 @@ export const AboutUsContent = () => {
     <>
       <h3>Our Story</h3>
       <p>
-        Classic Cinema has been the iconic home of cinema in the Dandenongs
-        since it opened with MGM’s David Copperfield on November 22, 1935. Since
-        its opening, it has been the home of every novelty movie from the
-        classic "Drakula", all the way to the most modern type of movies. The
-        cinema had to be rennovated a total of 3 times due to it sustaining
-        severe damage around 1985 from a very strong earthquake.
+        Retro Cinema has been the iconic home of cinema in the Athens since it
+        opened with MGM’s David Oster on November 22, 1935. Since its opening,
+        it has been the home of every novelty movie from the classic "Drakula",
+        all the way to the most retro type of movies. The cinema had to be
+        rennovated a total of 3 times due to it sustaining severe damage around
+        1985 from a very strong earthquake.
       </p>
       <p>
-        Nowadays, the Classic cinema is owned by Eddie Tamirand managed by
-        Danielle Burgess. With a total of 8 screens all equipped with the latest
-        dolby digital atmos sound systems, Classic Cinema offers an amazing
-        viewing experience for any kind of movie and for kids and adults alike.
+        Nowadays, the Retro cinema is owned by Eddie Paspas managed by Danielle
+        Rakis. With a total of 12 screens all equipped with the latest dolby
+        digital atmos sound systems, Retro Cinema offers an amazing viewing
+        experience for any kind of movie and for kids and adults alike.
       </p>
       <p>
-        Our main auditorium boasts 309 seats in a modern 21st century cinema.
-        Over summer, the Cameo Outdoor Cinema screens cult classic and first
-        release films amidst the picturesque surrounds of the Dandenong Ranges.
+        Our auditorium boasts 40 seats in a modern 21st century cinema. Over
+        summer, the Retro Outdoor Cinema screens cult classic and first release
+        films amidst the picturesque surrounds of the Athens Suburb.
       </p>
       <p>
-        On top of the crisp indoor experience, Classic Cinema operates 2 outdoor
+        On top of the crisp indoor experience, Retro Cinema operates 2 outdoor
         viewing areas for the majority of spring, summer and autumn. Adults can
         enjoy watching their favourite movies along with their families, while
         also enjoying a warm dinner in the beautiful outdoor scenery.
