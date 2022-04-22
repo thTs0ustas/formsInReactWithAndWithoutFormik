04ce3866a820b0e42c588258bce63c7e610db653
thts0ustas <th.ts0ustas@gmail.com>
2022-04-22 12:56:14 +0300

    added scroll to top

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { Provider } from "./model";
+import { ScrollToTop } from "./components/scrollToTop/ScrollToTop";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
+        <ScrollToTop />
         <App />
       </Provider>
     </BrowserRouter>
