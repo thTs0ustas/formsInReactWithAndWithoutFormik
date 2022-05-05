c65963da643dd0a4a6feace8bf428b6eb184774d
thts0ustas <th.ts0ustas@gmail.com>
2022-05-05 17:08:07 +0300

    minor Fixes

--- src/components/alertToast/Toast.js
+++ src/components/alertToast/Toast.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import { ToastContainer } from "./StyledComponents/ToastContainer";
 import { clearError } from "../../rModel";
 
-function AlertToast({ error: { message = "", time = "" } }) {
+function AlertToast({ error: { message, time } }) {
   const dispatch = useDispatch();
 
   return (
@@ -18,10 +18,22 @@ function AlertToast({ error: { message = "", time = "" } }) {
   );
 }
 
+AlertToast.defaultProps = {
+  error: {
+    message: "",
+    time: "",
+  },
+  message: "",
+  time: "",
+};
+
 AlertToast.propTypes = {
-  error: PropTypes.object.isRequired,
-  message: PropTypes.string.isRequired,
-  time: PropTypes.string.isRequired,
+  error: PropTypes.shape({
+    message: PropTypes.string,
+    time: PropTypes.string,
+  }),
+  message: PropTypes.string,
+  time: PropTypes.string,
 };
 
 export { AlertToast };
