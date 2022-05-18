c811a2c2c3730473d5bbfb743666dd30da1698a7
thts0ustas <th.ts0ustas@gmail.com>
2022-05-18 17:20:38 +0300

    changes in dependencies

--- src/components/admin/components/pagination/Pagination.js
+++ src/components/admin/components/pagination/Pagination.js
@@ -1,6 +1,6 @@
 import { Pagination } from "react-bootstrap";
 import { v4 as uuid4 } from "uuid";
-import React from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
 
 function PaginationBasic({ numberOfPages, page, setPage }) {
@@ -9,7 +9,11 @@ function PaginationBasic({ numberOfPages, page, setPage }) {
     .fill("")
     .forEach((_, i) =>
       items.push(
-        <Pagination.Item onClick={() => setPage(i)} key={uuid4()} active={page === i}>
+        <Pagination.Item
+          onClick={() => setPage(i)}
+          key={useMemo(() => uuid4(), [])}
+          active={page === i}
+        >
           {i + 1}
         </Pagination.Item>
       )
